c202f2f2ddf1ebab1b75da1f538f9dc87df29c99
sam77il <165700561+sam77il@users.noreply.github.com>
2025-09-29 11:14:22 +0200

    first

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class Game
+{
+    public string gameName;
+    public System.DateTime date;
+    public int difficulty;
+    public int level;
+    public int deaths;
+}
